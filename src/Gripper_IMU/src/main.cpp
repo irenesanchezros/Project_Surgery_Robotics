fecf01f4016ece3310c8fa465172c86eb5f3ee0c
juditbalague <jbalagbr64@alumnes.ub.edu>
2025-10-23 09:58:02 +0200

    Modified Gripper_IMU file

--- src/Gripper_IMU/src/main.cpp
+++ src/Gripper_IMU/src/main.cpp
@@ -1,25 +1,29 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include "MPU9250.h"
-#include <Wire.h>
-#include <ArduinoJson.h>  // No cal .hpp, la versió 7 manté compatibilitat amb .h
-#include <IMU_RoboticsUB.h>   // Nom de la llibreria custom
+#include <Wire.h> // Needed for I2C to read IMU
+#include <ArduinoJson.h> // Compatible amb versió 7.4.2
+#include <IMU_RoboticsUB.h>
 
 // Device ID
-const char *deviceId = "G1_Endo";
+const char *deviceId = "G1_Gri";
 
 // Wi-Fi credentials
 const char *ssid = "Robotics_UB";
 const char *password = "rUBot_xx";
 
+// Vibration motor settings
+const int vibrationPin = 23; // Pin for the vibration motor
+
 // Botons
-const int PIN_S3 = 14;
-const int PIN_S4 = 27;
-int s3Status = HIGH;
-int s4Status = HIGH;
+const int PIN_S1 = 14;
+const int PIN_S2 = 27;
+int s1Status = HIGH;
+int s2Status = HIGH;
 
 // UDP settings
-IPAddress receiverComputerIP(192, 168, 1, 15);
+IPAddress receiverESP32IP(192, 168, 1, 13); // IP of receiver ESP32
+IPAddress receiverComputerIP(192, 168, 1, 15); // IP of PC
 const int udpPort = 12345;
 WiFiUDP udp;
 
@@ -27,7 +31,7 @@ WiFiUDP udp;
 IMU imu;
 
 // Orientation data
-float Endo_roll = 0.0, Endo_pitch = 0.0, Endo_yaw = 0.0;
+float Gri_roll = 0.0, Gri_pitch = 0.0, Gri_yaw = 0.0;
 
 void connectToWiFi() {
   Serial.print("Connecting to Wi-Fi");
@@ -43,29 +47,35 @@ void connectToWiFi() {
 }
 
 void updateOrientation() {
-  // Llegeix FIFO del DMP i actualitza càlculs interns
+ // Llegeix FIFO del DMP i actualitza càlculs interns
   imu.ReadSensor();
   // Obté els angles (roll, pitch, yaw) via GetRPW()
   float* rpw = imu.GetRPW();
-  Endo_roll  = rpw[0];
-  Endo_pitch = rpw[1];
-  Endo_yaw   = rpw[2];
-  s3Status = digitalRead(PIN_S3);
-  s4Status = digitalRead(PIN_S4);
+  Gri_roll  = rpw[0];
+  Gri_pitch = rpw[1];
+  Gri_yaw   = rpw[2];
+  s1Status = digitalRead(PIN_S1);
+  s2Status = digitalRead(PIN_S2);
 }
 
 void sendOrientationUDP() {
   JsonDocument doc;
   doc["device"] = deviceId;
-  doc["roll"] = Endo_roll;
-  doc["pitch"] = Endo_pitch;
-  doc["yaw"] = Endo_yaw;
-  doc["s3"] = s3Status;
-  doc["s4"] = s4Status;
+  doc["roll"] = Gri_roll;
+  doc["pitch"] = Gri_pitch;
+  doc["yaw"] = Gri_yaw;
+  doc["s1"] = s1Status;
+  doc["s2"] = s2Status;
 
   char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer);
+  serializeJson(doc, jsonBuffer, sizeof(jsonBuffer));
 
+  // Send to ESP32 Servos
+  udp.beginPacket(receiverESP32IP, udpPort);
+  udp.write((const uint8_t*)jsonBuffer, strlen(jsonBuffer));
+  udp.endPacket();
+
+  // Send to Computer
   udp.beginPacket(receiverComputerIP, udpPort);
   udp.write((const uint8_t*)jsonBuffer, strlen(jsonBuffer));
   udp.endPacket();
@@ -76,12 +86,16 @@ void setup() {
   Wire.begin();
   delay(2000);
 
-  // Inicialitza IMU (amb DMP)
+ // Inicialitza IMU (amb DMP)
   imu.Install();
+
 
   connectToWiFi();
   udp.begin(udpPort);
-  Serial.println("UDP initialized.");
+  Serial.println("UDP initialized");
+
+  pinMode(PIN_S1, INPUT);
+  pinMode(PIN_S2, INPUT);
 }
 
 void loop() {
